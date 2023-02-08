a66854afc378f6c6598cbd2edd47b2280e9f897d
Chaas Kandregula <chaaskandregula@gmail.com>
2023-02-08 14:11:50 -0800

    added accelerometer code, buffering, linked w led

--- prevents_student_sleep_p1/prevents_student_sleep_p1.ino
+++ prevents_student_sleep_p1/prevents_student_sleep_p1.ino
@@ -1,94 +1,139 @@
 
-/*  Getting_BPM_to_Monitor prints the BPM to the Serial Monitor, using the least lines of code and PulseSensor Library.
- *  Tutorial Webpage: https://pulsesensor.com/pages/getting-advanced
- *
---------Use This Sketch To------------------------------------------
-1) Displays user's live and changing BPM, Beats Per Minute, in Arduino's native Serial Monitor.
-2) Print: "♥  A HeartBeat Happened !" when a beat is detected, live.
-2) Learn about using a PulseSensor Library "Object".
-4) Blinks the builtin LED with user's Heartbeat.
---------------------------------------------------------------------*/
-
-#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
-#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+#define USE_ARDUINO_INTERRUPTS true  // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>   // Includes the PulseSensorPlayground Library.
 
 //  Variables
-const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
-const int LED = LED_BUILTIN;   // The on-board Arduino LED, close to PIN 13.
+int accReadingsx[2];
+int accReadingsy[2];
+int accReadingsz[2];
+int x;                        // x axis variable
+int y;                        // y axis variable
+int z;                        // z axis variable
+const int PulseWire = 3;      // PulseSensor PURPLE WIRE connected to ANALOG PIN 3
+const int LED = LED_BUILTIN;  // The on-board Arduino LED, close to PIN 13.
 const int MAX_FAIL_COUNT = 10;
 int Threshold = 550;
-bool isValid (float BPM){
-  if (BPM > 0)
-      return (true);
-    else
-      return (false);
-}                  // Determine which Signal to "count as a beat" and which to ignore.
-                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
-                               // Otherwise leave the default "550" value. 
-                               
-PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
-  #include "RunningAverage.h"
-  
-  RunningAverage myRA(100); //change (100) to set interval of avg measurements
-  int trials = 0;
-  int failedReading = 0;
-   
-void setup() {   
-  int hrtoavg [100];  
-  Serial.begin(9600);          // For Serial Monitor
-  // Configure the PulseSensor object, by assigning our variables to it. 
-  pulseSensor.analogInput(PulseWire);   
-  pulseSensor.blinkOnPulse(LED);       //auto-magically blink Arduino's LED with heartbeat.
-  pulseSensor.setThreshold(Threshold);   
+int sleeptimer = 0;
+int i = 0;
+int ledState = LOW;
+const int ledPin = LED_BUILTIN;
 
-  // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
-   if (pulseSensor.begin()) {
-    Serial.println("_____________________________________________________________________________________________");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+bool isMoving(int dx, int dy, int dz) {
+  if (dx > 10)
+    return (true);
+  if (dy > 10)
+    return (true);
+  if (dz > 10)
+    return (true);
+  if (dx < 10)
+    if (dy < 10)
+      if (dz < 10)
+        return (false);
+}
+
+bool isValid(float BPM) {
+  if (BPM > 50)
+    return (true);
+  else
+    return (false);
+}  // Determine which Signal to "count as a beat" and which to ignore.
+   // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+   // Otherwise leave the default "550" value.
+
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+#include "RunningAverage.h"
+
+RunningAverage myRA(100);  //change (100) to set interval of avg measurements
+int trials = 0;
+int failedReading = 0;
+
+void setup() {
+  int hrtoavg[100];
+  Serial.begin(9600);  // For Serial Monitor
+  // Configure the PulseSensor object, by assigning our variables to it.
+  pulseSensor.analogInput(PulseWire);
+  pulseSensor.setThreshold(Threshold);
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal.
+  if (pulseSensor.begin()) {
+    Serial.println("_____________________________________________________________________________________________");  //This prints one time at Arduino power-up,  or on Arduino reset.
   }
-  myRA.clear(); // explicitly start clean
+  myRA.clear();  // explicitly start clean
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
 
- float myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
-                                               // "myBPM" hold this BPM value now. 
-  
-  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened". 
- // If test is "true", print a message "a heartbeat happened". 
-                         // Print the value inside of myBPM. 
-}
-  
-  
-          
-  if (isValid(myBPM) == true){
-    if (myBPM <= 90){
+  float myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+                                                  // "myBPM" hold this BPM value now.
+
+  if (pulseSensor.sawStartOfBeat()) {  // Constantly test to see if "a beat happened".
+  }
+
+
+
+  if (isValid(myBPM) == true) {
+    if (myBPM <= 90) {
       Serial.println("Are you sleeping?");
     }
     myRA.addValue(myBPM);
     trials++;
     Serial.print("Running Average: ");
-    Serial.println(myRA.getAverage()); 
+    Serial.println(myRA.getAverage());
     Serial.println(myBPM);
-  failedReading = 0;     
-    
-  
-  }
-  else {
+    failedReading = 0;
+
+  } else {
     failedReading++;
-    if (failedReading == MAX_FAIL_COUNT){
+    if (failedReading == MAX_FAIL_COUNT) {
       Serial.print("Still waiting. Did I mention that this sensor sucks? (this message pops up every ");
       Serial.print(MAX_FAIL_COUNT);
-      Serial.println(" failed attempts.)");
-      failedReading = 0;      
+      Serial.println(" failed attempts. )");
+      Serial.println("---------------------------------------------------------------------------------");
+      failedReading = 0;
     }
   }
-  
-  
-  
+
+  for (i = 0; i < 2; i++) {
+    accReadingsx[i] = x;
+    accReadingsy[i] = y;
+    accReadingsz[i] = z;
+    x = analogRead(2);  // read the second analog input pin
+    y = analogRead(1);  // read the first analog input pin
+    z = analogRead(0);  // read the zeroth analog input pin
+    Serial.println("--");
+    i = i++ % 2;
+  }
+
+  int diffx = accReadingsx[1] - accReadingsx[0];
+  int diffy = accReadingsy[1] - accReadingsy[0];
+  int diffz = accReadingsz[1] - accReadingsz[0];
+
+  if (isMoving(diffx, diffy, diffz) == true)
+    sleeptimer = 0;
+  else
+    sleeptimer++;
 
 
-  delay(1500); // considered best practice in a simple sketch.
+  if (sleeptimer >= 10){
+    Serial.println("l bozo");
+    if (ledState == LOW) {
+    ledState = HIGH;
+    } else {
+    ledState = LOW;
+  
+  }
+    digitalWrite(ledPin, ledState);
+  }
 
+
+
+  Serial.println(diffx);
+  Serial.println(diffy);
+  Serial.println(diffz);
+
+
+
+
+  delay(1500);  // considered best practice in a simple sketch.
 }
-
-  
